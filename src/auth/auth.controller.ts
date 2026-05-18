15a09659b45aa463ab0d4cfbce1821a30be816ed
Oscar Daniel Bueno Alvillar <oscar.bueno@adm.urbe.edu>
2026-05-18 13:48:12 -0400

    Auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
-
+import { AuthGuard } from '@nestjs/passport';
+import { LoginUserDto } from './dto/login-user.dto';
+import { Auth } from './decorators/auth.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -10,4 +12,20 @@ export class AuthController {
  async login(@Body() loginUserDto: any) {
    return this.authService.login(loginUserDto);
  }
+
+ @Get('profile')
+ @UseGuards(AuthGuard('jwt'))
+ getProfile(@Req() req:any) {
+  return req.user;
+}
+
+@Get('admin-panel')
+  @Auth('admin') 
+  getAdminData(@Req() req: any) {
+    return {
+      message: 'Panel Administrativo',
+      user: req.user
+    };
+}
+
 }
